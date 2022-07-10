2056c035d79afea4a84ebb003fef7cd43a9a931f
operation404 <operation404@gmail.com>
2022-07-10 11:10:59 -0700

    Added homebrew skills Alchemy and Enchant. Removed Work skill (text exists, commented out).

--- module/actor/character-sheet.js
+++ module/actor/character-sheet.js
@@ -22,7 +22,7 @@ export class WwnActorSheetCharacter extends WwnActorSheet {
       classes: ["wwn", "sheet", "actor", "character"],
       template: "systems/wwn/templates/actors/character-sheet.html",
       width: 730,
-      height: 625,
+      height: 650, // original 625
       resizable: false,
       tabs: [
         {
