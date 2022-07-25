b1240f5a3f692098074a8d3ab468f17f20a02aca
operation404 <operation404@gmail.com>
2022-07-24 20:42:12 -0700

    Added euclidean measurement for ruler

--- wwn.js
+++ wwn.js
@@ -109,6 +109,68 @@ Hooks.once("ready", async () => {
 
 });
 
+
+
+
+/* -------------------------------------------- 
+  Canvas Initialization                       
+  Caelan's addition for diagonal measuring
+ -------------------------------------------- */
+Hooks.on("canvasInit", function() {
+  // Extend Diagonal Measurement
+  canvas.grid.diagonalRule = "EUCL";
+  SquareGrid.prototype.measureDistances = measureDistances;
+});
+
+const measureDistances = function(segments, options={}) {
+  if ( !options.gridSpaces ) return BaseGrid.prototype.measureDistances.call(this, segments, options);
+
+  // Track the total number of diagonals
+  let nDiagonal = 0;
+  const rule = this.parent.diagonalRule;
+  const d = canvas.dimensions;
+
+  // Iterate over measured segments
+  return segments.map(s => {
+    let r = s.ray;
+
+    // Determine the total distance traveled
+    let nx = Math.abs(Math.ceil(r.dx / d.size));
+    let ny = Math.abs(Math.ceil(r.dy / d.size));
+
+    // Determine the number of straight and diagonal moves
+    let nd = Math.min(nx, ny);
+    let ns = Math.abs(ny - nx);
+    nDiagonal += nd;
+
+    // Alternative DMG Movement
+    if (rule === "5105") {
+      let nd10 = Math.floor(nDiagonal / 2) - Math.floor((nDiagonal - nd) / 2);
+      let spaces = (nd10 * 2) + (nd - nd10) + ns;
+      return spaces * canvas.dimensions.distance;
+    }
+
+    // Euclidean Measurement
+    else if (rule === "EUCL") {
+	  return Math.round(Math.hypot(nx, ny) * canvas.scene.data.gridDistance);
+	  //return Math.round(Math.hypot(nx, ny) * canvas.scene.data.gridDistance * 100) / 100;
+    }
+
+    // Standard PHB Movement
+    else return (ns + nd) * canvas.scene.data.gridDistance;
+  });
+};
+
+
+
+
+
+
+
+
+
+
+
 // License and KOFI infos
 Hooks.on("renderSidebarTab", async (object, html) => {
   if (object instanceof ActorDirectory) {
